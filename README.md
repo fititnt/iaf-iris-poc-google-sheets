05d74456f61ecebdcc8f647d1041040be75ca1c2
Emerson Rocha Luiz <emerson@alligo.com.br>
2017-05-22 18:02:44 -0300

    poc3: criado baseado em poc2

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Veja [a documentação do exemplo simples](simples/).
 - A planilha publicamente editavel
   - <https://docs.google.com/spreadsheets/d/1jNVgLi_dRPO413EmLBjr5rNTWUOt2On67OOJ6gdr5xE/edit>
 
-# Prova de Conceito 2: Entrada por voz com comando salvo em planilha
+## Prova de Conceito 2: Entrada por voz com comando salvo em planilha
 
 Veja [a documentação da prova de conceito 2](poc2/).
 
@@ -17,3 +17,8 @@ Veja [a documentação da prova de conceito 2](poc2/).
   - <https://fititnt.github.io/iaf-iris-poc-google-sheets/poc2/>
 - A planilha publicamente editavel
   - <https://docs.google.com/spreadsheets/d/1Xq2itPNgnnsMrOOeS3X8lWttnh9EBDpeGIYN0A8UBss/edit>
+
+## Prova de Conceito 3: Entrada por voz com comando salvo em planilha, variante
+Acesse <https://fititnt.github.io/iaf-iris-poc-google-sheets/poc3/> para
+ver a versão atual. O formulário você deverá cadastrar com botão "Lembrar meus
+dados".
---
+++ poc3/macro-planilha.js
@@ -0,0 +1,80 @@
+// 1. Crie uma nova planilha em https://docs.google.com/spreadsheets/
+// 2. Nesta planilha, adicione na primeira linha e coluna (A1) o valor 
+//   "Timestamp" e ao seu lado (B1) o valor "instrucao" (não use aspas)
+// 3. Clique em "Ferramentas > Editor de Scripts" no seu Google Planilhas.
+//    Irá abrir um editor de scripts
+// 4. Copie e cole todo esse código (arquivo macro-planilha.js) para o editor
+//    de scripts que abriu
+// 5. Salve (clique no "disquete", ou aperte as telas 'Ctrl + S' se não sabe
+//    o que é um ícone de disquete). Qualquer nome de projeto serve. Sugerido
+//    "poc2"
+// 6. Em "Select function" selecione "Setup".
+// 7. Clique em "Run" (é um triângulo apontando para direita, entre um relógio
+//    e um inseto)
+// 8. Clique em "Review Permissions" na caixa que irá aparecer. Selecione sua
+//    conta pessoal
+// 9. Publish > Deploy as web app
+//    - "Project version" pode ser valor 1
+//    - Execute the app as: "me" (vai rodar como se fosse você)
+//    - Who has access to the app: "Anyone, even anonymous"
+// 10. Copie a URL de "Current web app URL:" e cole na respectíva área da sua interface
+
+var SCRIPT_PROP = PropertiesService.getScriptProperties(); // new property service
+
+// If you don't want to expose either GET or POST methods you can comment out the appropriate function
+function doGet(e) {
+  return handleResponse(e);
+}
+
+function doPost(e) {
+  return handleResponse(e);
+}
+
+function handleResponse(e) {
+  // shortly after my original solution Google announced the LockService[1]
+  // this prevents concurrent access overwritting data
+  // [1] http://googleappsdeveloper.blogspot.co.uk/2011/10/concurrency-and-google-apps-script.html
+  // we want a public lock, one that locks for all invocations
+  var lock = LockService.getPublicLock();
+  lock.waitLock(30000);  // wait 30 seconds before conceding defeat.
+
+  try {
+    // next set where we write the data - you could write to multiple/alternate destinations
+    var doc = SpreadsheetApp.openById(SCRIPT_PROP.getProperty("key"));
+    var sheet = doc.getSheetByName(e.parameter['pagina']); //var sheet = doc.getSheetByName(SHEET_NAME);
+
+    // we'll assume header is in row 1 but you can override with header_row in GET/POST data
+    var headRow = e.parameter.header_row || 1;
+    var headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0];
+    var nextRow = sheet.getLastRow() + 1; // get next row
+    var row = [];
+    // loop through the header columns
+    for (i in headers) {
+      if (headers[i] == "id") {
+        row.push(sheet.getLastRow());
+      } else if (headers[i] == "created") {
+        row.push(new Date());
+      } else if (headers[i] != "pagina" && headers[i] != "webapp-url") {
+        row.push(e.parameter[headers[i]]);
+      }
+    }
+    // more efficient to set values as [][] array than individually
+    sheet.getRange(nextRow, 1, 1, row.length).setValues([row]);
+    // return json success results
+    return ContentService
+      .createTextOutput(JSON.stringify({ "result": "success", "row": nextRow }))
+      .setMimeType(ContentService.MimeType.JSON);
+  } catch (e) {
+    // if error return this
+    return ContentService
+      .createTextOutput(JSON.stringify({ "result": "error", "error": e }))
+      .setMimeType(ContentService.MimeType.JSON);
+  } finally { //release lock
+    lock.releaseLock();
+  }
+}
+
+function setup() {
+  var doc = SpreadsheetApp.getActiveSpreadsheet();
+  SCRIPT_PROP.setProperty("key", doc.getId());
+}
